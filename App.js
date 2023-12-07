1e5ee7a7304d9491e5513086897b5406a855f09c
Tahmid <tahmidsheikh610@gmail.com>
2023-12-07 10:15:44 -0500

    app config

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomePage from './src/components/HomePage';
-import InstructionsCreateMeet from './src/components/InstructionsCreateMeet';
-import JoinMeet from './src/components/JoinMeet';
-import CreateMeetDetails from './src/components/CreateMeetDetails';
-import CreateMeet from './src/components/CreateMeet';
-import JoinMeetDetails from './src/components/JoinMeetDetails';
-import Meet from './src/components/Meet';
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-// import firebase from 'firebase/app';
-// import 'firebase/database'; // Import the database module
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomePage from './src/components/HomePage'
+import InstructionsCreateMeet from './src/components/InstructionsCreateMeet'
+import JoinMeet from './src/components/JoinMeet'
+import CreateMeetDetails from './src/components/CreateMeetDetails'
+import CreateMeet from './src/components/CreateMeet'
+import JoinMeetDetails from './src/components/JoinMeetDetails'
+import Meet from './src/components/Meet'
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
+// import firebase from 'firebase/app'
+// import 'firebase/database' // Import the database module
 
 
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 
@@ -38,5 +38,5 @@ export default function App() {
         <Stack.Screen name="JoinMeetDetails" component={JoinMeetDetails} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
